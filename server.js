f425f27c9d8afd965f467e6471e52250aa3a8177
gagangeekologix <gagan.geekologix@gmail.com>
2023-08-08 11:52:54 +0530

    new commit

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const dotenv = require("dotenv").config();
-
+const cors = require('cors');
 const mongoose = require('mongoose');
 const BusStop = require('./models/busStop');
 const Bus = require('./models/bus');
 const connectDb = require('./Db/dbConnection');
 connectDb();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", require("./routes/busRoutes"));
 app.use("/api", require("./routes/busStopRoutes"));
